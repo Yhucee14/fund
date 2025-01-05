74b16967d57683f3c287f0299fe8897a84d25fbc
Yhucee14 <uchechukwu7814@gmail.com>
2025-01-04 23:33:57 -0800

    deployed to zksync

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -7,12 +7,13 @@ pragma solidity 0.8.24;
 
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
+
 library PriceConverter {
      function getPrice() internal view returns (uint256){
-        //Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
-        AggregatorV3Interface dataFeed = AggregatorV3Interface(0xfEefF7c3fB57d18C5C6Cdd71e45D2D0b4F9377bF);
-        (, int256 answer ,,,) = dataFeed.latestRoundData();
-//
+       
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0xfEefF7c3fB57d18C5C6Cdd71e45D2D0b4F9377bF);
+        (, int256 answer ,,,) = priceFeed.latestRoundData();
+
         return uint256 (answer * 1e10);
     }
 
